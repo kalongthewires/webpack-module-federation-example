8eea3d71b8938261cb964dabf807f2993ec10b3b
kalongthewires <kim.hamper@gmail.com>
2022-07-22 08:58:59 -0400

    Clean out all files

--- container/src/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import { mount as mountProducts } from "products/Products";
-import { mount as mountCart } from "cart/Cart";
-
-mountProducts(document.getElementById("host-products"));
-mountCart(document.getElementById("host-cart"));
